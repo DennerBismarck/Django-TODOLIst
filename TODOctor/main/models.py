bda9b6a023f89346e0b53bf3fe0486f458d9a4cb
Denner Bismarck <dennerbismarck@gmail.com>
2024-05-10 09:04:14 -0300

    Feat: Tarefa Model creation and migration #2

--- TODOctor/main/models.py
+++ TODOctor/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+
+    class situacoes_possiveis(models.TextChoices):
+        NOVA = 'NO', 'Nova'
+        EM_ANDAMENTO = 'AN', 'Em andamento'
+        CONCLUIDA = 'CO', 'Concluida'
+        CANCELADA = 'CA', 'Cancelada' 
+
+    titulo = models.CharField(max_length=100, null=False)
+    descricao = models.CharField(max_length=250, blank=True)
+    prazo = models.DateField()
+    data_conclusao = models.DateField(null=True, blank=True)
+    situacao = models.CharField(choices=situacoes_possiveis, max_length=2)
+    
