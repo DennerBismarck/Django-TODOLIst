f263aa659468f093f2d8f804bc4ef1e903bc614f
Denner Bismarck <dennerbismarck@gmail.com>
2024-05-12 10:27:08 -0300

    Feat: Tarefas basic read All

--- TODOctor/api/views.py
+++ TODOctor/api/views.py
@@ -1,9 +1,18 @@
 #API Views
-from rest_framework.response import Response #Python or Serialized data to JSON data
-from rest_framework.decorators import api_view #Transforms function based views into APIView
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from main.models import Tarefa
+from .serializers import TarefaSerializer
+
 
 @api_view(['GET'])
 def ConfigTest(request):
     test = {'Título':'Desenvolver API', 'Descrição': 'Desenvolver API de To-Do List', 'Prazo':'09/05/2024', 'Data de Conclusão':'09/05/2024', 'Situação':'Em Andamento'}
     return Response(test)
+
+@api_view(['GET'])
+def TarefaGetAll(request):
+    tarefas = Tarefa.objects.all()
+    serializer = TarefaSerializer(tarefas, many = True)
+    return Response(serializer.data)
 
