26c4d57d0679165f0f646e94ee154fa5c27abeaf
Denner Bismarck <dennerbismarck@gmail.com>
2024-05-15 09:53:01 -0300

    Fix: Static fix

--- TODOctor/TODOctor/settings.py
+++ TODOctor/TODOctor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e#n=@y%()$l^r8c54xa0&rp)h#+=-s$jwq88n6m(n#r+tsmxg='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = '/app/static/'
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
